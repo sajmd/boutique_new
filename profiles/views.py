3581a32f000593f725debdf0500f953f8d77c311
sajmd <sajidjava90@gmail.com>
2021-07-24 23:19:51 +0000

    Added profiles App

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-
-from .models import UserProfile
+from django.shortcuts import render
 
 
 def profile(request):
     """ Display the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
-    context = {
-        'profile': profile,
-    }
+    context = {}
 
     return render(request, template, context)
